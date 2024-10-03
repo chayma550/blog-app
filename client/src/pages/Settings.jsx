9801b0c51d6f89491e634e414623d3f05b437a88
chayma550 <rachayma@gmail.com>
2024-10-03 13:38:14 +0200

    Update Client

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -73,7 +73,6 @@ const handleSubmit=async(e)=>{
             <input
               id="fileInput"
               type="file"
-              style={{ display: "none" }}
               className="settingsPPInput"
               onChange={(e) => setFile(e.target.files[0])}
 
